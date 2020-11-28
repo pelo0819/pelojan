36164d1fdf67582ed395a16cc533e8900ae5d5c7
pelo0819 <canvas.pelo@gmail.com>
2020-11-28 13:33:58 +0900

    update

---
+++ tests/fsa/formatStringAttack.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char buf[100];
+    char *secret;
+    FILE *fp;
+
+    secret = malloc(2000);
+    printf("[+] secret = %p\n", secret);
+
+    fp = fopen("./resources/fsc_test.txt", "r");
+    fread(secret, 1, 2000, fp);
+    fclose(fp);
+    printf("length = %d\n", strlen(secret));
+
+    strncpy(buf, argv[1], sizeof(buf));
+    printf(buf);
+    printf("\n");
+
+    free(secret);
+    return 0;
+}
