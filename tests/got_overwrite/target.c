efa4c3a6f953d551bf2e5189cee65c4ede81c7f7
pelo0819 <canvas.pelo@gmail.com>
2020-11-28 19:29:29 +0900

    add got_overwrite

---
+++ tests/got_overwrite/target.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char buf[100];
+    printf("[+] buf = %p\n", buf);
+    strncpy(buf, argv[1], sizeof(buf));
+    printf(buf);
+    putchar("\n");
+    return 0;
+}
