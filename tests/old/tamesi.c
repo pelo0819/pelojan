caf1bbf5eb1db2e932994039e2a9fa4e83e53542
飛田大二郎 <pelo.canvas@gmail.com>
2020-12-05 11:31:55 +0900

    update

--- tests/old/tamesi.c
+++ tests/old/tamesi.c
@@ -4,24 +4,29 @@
 
 int main(int argc, char *argv[])
 {
-    char* str = "AAA";
-    printf("str:%s, ptr:%p\n", str, str);
-    int address = str;
-    printf("%s.%08x.%08x.%08x.%08x.%08x\n", address);
-    printf("%s.%5$08x\n", address);
-    printf("\n");
+    // char* str = "AAA";
+    // printf("str:%s, ptr:%p\n", str, str);
+    // int address = str;
+    // printf("%s.%08x.%08x.%08x.%08x.%08x\n", address);
+    // printf("%s.%5$08x\n", address);
+    // printf("\n");
 
-    int ints[] ={1,4,6,8,2};
-    int addrInts0 = ints[0];
-    printf("ints pointer:%p\n", ints);
+    // int ints[] ={1,4,6,8,2};
+    // int addrInts0 = ints[0];
+    // printf("ints pointer:%p\n", ints);
     
-    printf("ints");
-    printf(ints);
-    printf('\n');
+    // printf("ints");
+    // printf(ints);
+    // printf('\n');
     
-    int a = atoi(argv[1]);
-    printf("aa%d\n", a);
-    printf(argv[1]);
+    // int a = atoi(argv[1]);
+    // printf("aa%d\n", a);
+    // printf(argv[1]);
+
+    int *a;
+    int b = 2;
+    a = &b;
+    printf("[*] a:%d, a_ptr:%p" , *a, a);
 
     return 0;
 }
