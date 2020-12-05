539f0604611f5e6000f671e4be327f433fe7c35d
飛田大二郎 <pelo.canvas@gmail.com>
2020-12-05 09:55:14 +0900

    update tamesi

--- tests/old/tamesi.c
+++ tests/old/tamesi.c
@@ -4,30 +4,19 @@
 
 int main(int argc, char *argv[])
 {
-    char* str = "hello";
+    char* str = "AAA";
     printf("str:%s, ptr:%p\n", str, str);
     int address = str;
     printf("%s.%08x.%08x.%08x.%08x.%08x\n", address);
+    printf("%s.%5$08x\n", address);
     printf("\n");
 
     int ints[] ={1,4,6,8,2};
-    printf("%d\n", argc);
-    // printf("%d.%08x.%08x.%08x.%08x\n", ints[0]);
-    // printf("%d%n", ints[0], ints[1]);
+    int addrInts0 = ints[0];
+    printf("%p\n", ints);
 
-    int addr = str;
-    // printf("sss%n\n", &addr);
-
-    // char aa = 
     int a = atoi(argv[1]);
     printf("aa%d\n", a);
-
-    printf("%d\n", addr);
-    printf("%d\n", ints[0]);
-    printf("%d\n", ints[1]);
-    printf("%d\n", ints[2]);
-    printf("%d\n", ints[3]);
-
     printf(argv[1]);
 
     return 0;
