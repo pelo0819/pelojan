c3501b191083f1bda568e5ae518ef23ef4eaa2c8
root <root@localhost.localdomain>
2020-12-12 13:15:46 +0900

    update

--- tests/got_overwrite/target.c
+++ tests/got_overwrite/target.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+int test()
+{
+    puts("test");
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     char buf[100];
     printf("[+] buf = %p\n", buf);
     strncpy(buf, argv[1], sizeof(buf));
     printf(buf);
-    putchar("\n");
+    putchar('\n');
     return 0;
 }
